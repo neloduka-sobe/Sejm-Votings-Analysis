5e7a489276bf0d5c111911c6b7c331be5dd9f427
neloduka-sobe <borys.langowicz@mail.utoronto.ca>
2024-04-23 16:11:28 -0400

    Add downloading club data

--- download_data.R
+++ download_data.R
@@ -5,13 +5,30 @@
 # Load required libraries
 library(httr)
 library(jsonlite)
+library(dplyr)
 
 API_URL <- "https://api.sejm.gov.pl"
 TERM <- "10"
 
 # Test run
-PATH = "/sejm/term"
-url = paste0(API_URL, PATH, TERM)
-res = GET(url)
-data  = fromJSON(rawToChar(res$content))
-data
+#PATH = "/sejm/term"
+#url = paste0(API_URL, PATH, TERM)
+#res = GET(url)
+#data  = fromJSON(rawToChar(res$content))
+#data
+
+# Loading clubs data
+url <- paste0(API_URL, "/sejm/term", TERM, "/clubs")
+
+# Error check
+res <- GET(url)
+if (res$status != 200) {
+  stop("Error loading clubs data")
+}
+clubs <- fromJSON(rawToChar(res$content))
+clubs_dataframe <- as.data.frame(clubs) %>% select(id, membersCount, name)
+write.csv(clubs_dataframe, "./Data/clubs.csv", row.names=FALSE)
+
+# Loading MP data #TODO
+
+# Loading votings data #TODO
