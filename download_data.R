812cf89a82372e6b66de449554af838108b7fc87
neloduka-sobe <borys.langowicz@mail.utoronto.ca>
2024-04-23 16:30:32 -0400

    Add MP data

--- download_data.R
+++ download_data.R
@@ -6,6 +6,7 @@
 library(httr)
 library(jsonlite)
 library(dplyr)
+library(lubridate)
 
 API_URL <- "https://api.sejm.gov.pl"
 TERM <- "10"
@@ -17,18 +18,27 @@ TERM <- "10"
 #data  = fromJSON(rawToChar(res$content))
 #data
 
+
 # Loading clubs data
 url <- paste0(API_URL, "/sejm/term", TERM, "/clubs")
 
-# Error check
 res <- GET(url)
 if (res$status != 200) {
   stop("Error loading clubs data")
 }
 clubs <- fromJSON(rawToChar(res$content))
-clubs_dataframe <- as.data.frame(clubs) %>% select(id, membersCount, name)
+clubs_dataframe <- as.data.frame(clubs)
 write.csv(clubs_dataframe, "./Data/clubs.csv", row.names=FALSE)
 
-# Loading MP data #TODO
+
+# Loading MP data
+url <- paste0(API_URL, "/sejm/term", TERM, "/MP")
+res <- GET(url)
+if (res$status != 200) {
+  stop("Error loading MP data")
+}
+mp <- fromJSON(rawToChar(res$content))
+mp_dataframe <- as.data.frame(mp)
+write.csv(mp_dataframe, "./Data/mp.csv", row.names=FALSE)
 
 # Loading votings data #TODO
